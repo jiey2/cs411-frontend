636a0a070c134cc43a49ab1b2f088297f51c35b9
jiey2 <jiey2@illinois.edu>
2020-08-02 02:38:19 -0500

    August 1 end work stable

--- src/ItemDetails.js
+++ src/ItemDetails.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect } from 'react';
 import { Image, Container } from 'react-bootstrap'
 import { Rate, Statistic, Row, Col } from 'antd';
 import { HeartOutlined, HeartTwoTone } from '@ant-design/icons';
@@ -18,14 +18,25 @@ class ItemDetails extends Component {
         this.state = {
             loading: true,
             itemDetails: null,
+            itemName: null,
             like: 0,
         }
     }
 
-    setRating(myRate) {
-        this.state.like += myRate; 
-        console.log(this.state.like);
-
+    async setRating(myRate) {
+        this.state.like += myRate;
+        console.log([this.state.itemName,myRate])
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ 
+                ItemName: this.state.itemName,
+                Like: myRate
+            })
+        }; 
+        const url = "http://api.996.com.de/item/rate";
+        const response = await fetch(url, requestOptions);
+        console.log(response);
     }
 
     async componentDidMount() {
@@ -33,9 +44,10 @@ class ItemDetails extends Component {
         const url = `http://api.996.com.de/item/${encodeURIComponent(itemName)}`;
         const response = await fetch(url);
         const data = await response.json();
-        // console.log(data);
+        console.log(data);
         // console.log(data.data[0][2]);
         this.setState({ itemDetails: data });
+        this.setState({ itemName: data.data[0][0], like: data.data[0][4]})
         this.setState({ loading: false });
     }
 
@@ -51,22 +63,20 @@ class ItemDetails extends Component {
             return (
                 <Container>
                     <h2>Item Details</h2>
-                    
+
                     <Row>
-                        <Col span={12}>  
+                        <Col span={12}>
                             <StyleIcon src={this.state.itemDetails.data[0][2]}></StyleIcon>
                             <h4>{this.state.itemDetails.data[0][0]}</h4>
                             <h6>{this.state.itemDetails.data[0][3]}</h6>
                         </Col>
                         <Col>
-                            <Statistic title='Like' value={this.state.like}/>
-                            <Rate 
-                                character={<HeartOutlined />} 
+                            <Statistic id="likenum" title='Like' value={this.state.like} />
+                            <Rate
+                                character={<HeartOutlined />}
                                 onChange={(myRate) => this.setRating(myRate)}
-                                allowClear={false} 
+                                allowClear={false}
                             />
-                        </Col>
-                        <Col>
                         </Col>
                     </Row>
                     <hr />
